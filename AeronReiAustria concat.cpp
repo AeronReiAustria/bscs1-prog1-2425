27871d2b3d8d7959b3954a0650d8bd372ef3d707
AeronRei <Golden_Aeron945@yahoo.com>
2024-09-24 23:12:43 +0800

    Create AeronReiAustria concat.cpp

---
+++ AeronReiAustria concat.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string fullname = "Aeron Rei A Austria";
+    string course = "A.C.T 1";
+    cout << fullname << endl;
+    cout << course << endl;
+    return 0;
+}
